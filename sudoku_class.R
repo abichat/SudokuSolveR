749a0ef3b9157905eeafc885614a363b66e6a818
abichat <a.bichat@yahoo.fr>
2018-03-14 19:09:49 +0100

    En fait ça marche pas :/

--- sudoku_class.R
+++ sudoku_class.R
@@ -27,7 +27,7 @@ grid <-
                 is.na() %>% 
                 which() %>% 
                 tibble(p = .) %>% 
-                mutate(n = map(p, ~ authorized_numbers(self$vec, .)),
+                mutate(n = map(p, ~ authorized_numbers(self$vec, .)),  # From functions file
                        l = map_int(n, length))
               
               if (nrow(self$df_empty_cases) == 0) {
@@ -92,6 +92,10 @@ grid <-
             
             solve = function(){
               
+              if (self$status == "wrong") {
+                return("No solution found")
+              }
+              
               while (self$status == "unambiguous") {
                 self$fill_unambiguous()
               }
@@ -104,7 +108,8 @@ grid <-
               }
               
               if (self$status == "complete") {
-                return(self$vec)
+                # return(self$vec)
+                return(self)
               }
               
               return("No solution found")
@@ -112,18 +117,16 @@ grid <-
             
             print = function(){
               base::print(self$vec)
-              # base::print(plot_matrix(self$vec))
+              base::print(plot_matrix(self$vec))       # From function files
               base::print(paste("Statut:", self$status))
             }
           )
           )
 
 
-G1 <- grid$new(V_complete)
-G1
-
 G2 <- grid$new(V_almostcomp) 
 G2
+G2$solve()
 G2$df_empty_cases
 G2$fill_unambiguous()
 G2
@@ -133,13 +136,7 @@ G2
 G3 <- grid$new(V_impossible)
 G3
 G3$df_empty_cases
-# G3$fill_unambiguous()
-# G3
-# G3$fill_unambiguous()
-# G3
-# G3$df_empty_cases %>% View
-# G3$fill_unambiguous()
-# G3
+
 G3$solve_only_unambiguous()
 G3
 
@@ -151,31 +148,12 @@ G4
 
 
 G5 <- grid$new(V_hardcore)
-G5$solve_only_unambiguous()
 G5
-
-G5$solve()
+G5$solve() # Ça ne marche pas !!! :(
 
 G5$create_children()
 G5$children
 G5$children[[2]]$solve()
 
 G6 <- G5$children[[1]]
-G6$solve_only_unambiguous()
 G6$solve()
-
-# df <- G5$df_empty_cases %>% arrange(l) %>% .[1,]
-# df$p
-# df$n
-# 
-# G5$fill_cases(df$p, 4)$vec
-# (G5$fill_cases(df$p, 4))$vec
-# grid$new((G5$fill_cases(df$p, 4))$vec)
-# 
-# G5$fill_cases(df$p, 4)
-# G5$vec
-# 
-# 
-# 
-# L <- map(as.list(unlist(df$n)), ~ grid$new(G5$fill_cases(df$p, .)$vec))
-# 
