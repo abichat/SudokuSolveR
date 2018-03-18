c651e9b97067d169608635dbf4e68381acf77115
abichat <a.bichat@yahoo.fr>
2018-03-18 11:52:49 +0100

    my_color & as.character

--- sudoku_shiny.R
+++ sudoku_shiny.R
@@ -4,6 +4,8 @@ library(shinymaterial)
 choices_grid <- c("-" = NA, "1" = 1, "2" = 2, "3" = 3, "4" = 4,
                   "5" = 5, "6" = 6, "7" = 7, "8" = 8, "9" = 9)
 
+my_color <- "indigo"
+
 my_material_dropdown <- function(chnumber){
   material_dropdown(
     input_id = chnumber, label = NULL,
@@ -11,6 +13,7 @@ my_material_dropdown <- function(chnumber){
 }
 
 my_material_column <- function(vecchnumber){
+  vecchnumber <- as.character(vecchnumber)
   material_column(width = 1, 
                   my_material_dropdown(vecchnumber[1]),
                   my_material_dropdown(vecchnumber[2]),
@@ -29,27 +32,27 @@ my_material_column <- function(vecchnumber){
 # Wrap shinymaterial apps in material_page
 ui <- material_page(
   title = "SudokuSolver",
-  nav_bar_color = "light-blue",
+  nav_bar_color = my_color,
   tags$h4("Fill your grid:"),
   material_row(
-    material_column(width = 0),
-    my_material_column(as.character(1:9)),
-    my_material_column(as.character(10:18)),
-    my_material_column(as.character(19:27)),
     material_column(width = 1),
-    my_material_column(as.character(28:36)),
-    my_material_column(as.character(37:45)),
-    my_material_column(as.character(46:54)),
+    my_material_column(1:9),
+    my_material_column(10:18),
+    my_material_column(19:27),
     material_column(width = 1),
-    my_material_column(as.character(55:63)),
-    my_material_column(as.character(64:72)),
-    my_material_column(as.character(73:81))
+    my_material_column(28:36),
+    my_material_column(37:45),
+    my_material_column(46:54),
+    material_column(width = 1),
+    my_material_column(55:63),
+    my_material_column(64:72),
+    my_material_column(73:81)
     ),
   br(),
   material_row(
     material_column(align = "center", width = 12,
                     material_button(input_id = "solve", label = "SOLVE",
-                                    color = "light-blue", depth = 5))
+                                    color = my_color, depth = 5))
   ),
   br()
 )
